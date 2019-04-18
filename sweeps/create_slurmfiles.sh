6f95ccd37eeda5a50151971fb48baa2fe07a9e9f
Frances Ding <frances.ding@gmail.com>
2019-04-18 02:02:15 -0400

    sbatch commands for new run with agreement data

--- sweeps/create_slurmfiles.sh
+++ sweeps/create_slurmfiles.sh
@@ -4,14 +4,19 @@
 base="/Users/Frances/Documents/seas-fellowship/rvr/sweeps/"
 #base="/n/home06/fding/rvr/sweeps/"
 #file="/Users/Frances/Documents/seas-fellowship/rvr/sweeps/runp1_2_sweep/commands.sh"
-sweep="runp1_2_sweep"
+sweep="runagree_sweep"
 file=${base}${sweep}"/commands.sh"
 out=${base}${sweep}"/array_commands/"
 
 count=0
+subcount=0
 
 while IFS= read -r line
 do
 	echo $line >> $out$count.sh
-	let "count++"
+	let "subcount++"
+	if [ $subcount = 5 ]; then
+	    let "count++"
+	    subcount=0
+	fi
 done < $file
