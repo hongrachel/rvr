30dce86588c536e66eb388b45d131130bbfe6fcf
Frances Ding <fding@boslogin03.rc.fas.harvard.edu>
2019-05-03 13:38:17 -0400

    results from just one product term interaction

--- src/testing/get_best_epochs.py
+++ src/testing/get_best_epochs.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
     #runp_dirs = [(runp_dir.format(sweepname, gamma, beta, seed), gamma, beta) for gamma, beta in itertools.product(fair_coeffs, recon_coeffs)]
 
 
-    sweepname = 'runagree_interact_042919_prod_thresh'
+    sweepname = 'runagree_interact_050219_prod'
     seed = args.seed
     runagree_dir = '{}/data--runagree--model_adim-4--model_class-MultiEqOddsUnweightedWassGan--model_fair_coeff-{}--model_recon_coeff-{}--model_seed-{}'
     runagree_dirs = [(runagree_dir.format(sweepname, gamma, beta, seed), gamma, beta) for gamma, beta in itertools.product(fair_coeffs, recon_coeffs)]
