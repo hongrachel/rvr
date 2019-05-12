af226df9bb349e8879cd96cfc279361b1966d047
Frances Ding <fding@boslogin04.rc.fas.harvard.edu>
2019-05-12 18:01:26 -0400

    new results from 1 product interaction setup, random seed 1

--- src/testing/get_best_epochs.py
+++ src/testing/get_best_epochs.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
     #runp_dirs = [(runp_dir.format(sweepname, gamma, beta, seed), gamma, beta) for gamma, beta in itertools.product(fair_coeffs, recon_coeffs)]
 
 
-    sweepname = 'runagree_no_interact_050919'
+    sweepname = 'runagree_interact_051019_prod'
     seed = args.seed
     runagree_dir = '{}/data--runagree--model_adim-4--model_class-MultiEqOddsUnweightedWassGan--model_fair_coeff-{}--model_recon_coeff-{}--model_seed-{}'
     runagree_dirs = [(runagree_dir.format(sweepname, gamma, beta, seed), gamma, beta) for gamma, beta in itertools.product(fair_coeffs, recon_coeffs)]
