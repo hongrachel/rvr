ca34d88a79d6acba399b7b7833fc1e38c5c7df14
Frances Ding <frances.ding@gmail.com>
2019-07-22 12:17:21 -0700

    new parameters for mnist sweep to balance the loss function components better

--- sweeps/mnist_sweep/array_commands/28.sh
+++
@@ -1,1 +0,0 @@
-python src/run_laftr.py sweeps/mnist_sweep/config.json --dirs odyssey --data mnist -o model.adim=3,model.class=MultiEqOddsUnweightedWassGan,model.fair_coeff=3.0,model.recon_coeff=0.005,model.seed=3,exp_name="mnist_TEST/data--mnist--model_adim-3--model_class-MultiEqOddsUnweightedWassGan--model_fair_coeff-3_0--model_recon_coeff-0_005--model_seed-3"
